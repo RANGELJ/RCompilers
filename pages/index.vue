7f2d68dab934fdd942d5c8fff4fc4df3040b0d1f
Rangel <georgeranpe@gmail.com>
2018-04-17 17:15:45 -0500

    Regulex Parser added

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
 <v-layout row justify-center align-center>
-<v-flex xs12>
-    <h1>Here</h1>
-</v-flex>
+    <v-flex xs12>
+        <v-text-field label="Reguex" v-model="reguexA"></v-text-field>
+        <v-btn @click="testNFA">Test</v-btn>
+    </v-flex>
 </v-layout>
 </template>
 
 <script>
+import Parser from "../lib/regulex/Parser";
 export default {
+    data () {
+        return {
+            reguexA: ""
+        };
+    },
+    methods: {
+        testNFA () {
+            console.log(Parser.parse(this.reguexA));
+        }
+    }
 };
 </script>
